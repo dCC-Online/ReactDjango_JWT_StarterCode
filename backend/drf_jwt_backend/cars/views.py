ad9039e23f187c286c2a47494bce418332aece02
JJ <jj.vega86@gmail.com>
2022-03-15 11:37:16 -0400

    Removed unused imports from cars/views.py + updated Postman collection

--- backend/drf_jwt_backend/cars/views.py
+++ backend/drf_jwt_backend/cars/views.py
@@ -1,11 +1,9 @@
 from rest_framework import status
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from .models import Car
 from .serializers import CarSerializer
-from django.contrib.auth.models import User
 
 # <<<<<<<<<<<<<<<<< EXAMPLE FOR STARTER CODE USE <<<<<<<<<<<<<<<<<
 
